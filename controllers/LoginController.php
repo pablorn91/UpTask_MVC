e76e1117fb56e53c4e7df4e6ab90cb7d095f555d
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-01-13 16:09:13 -0400

    Vista login creada

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,14 +1,19 @@
 <?php
 namespace Controllers;
 
+use MVC\Router;
+
 class LoginController {
 
-    public static function login() {
-        echo "Desde Login";
+    public static function login(Router $router) {
 
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
             
         }
+
+        $router->render('auth/login', [
+            'titulo' => 'Iniciar Sesión'
+        ]);
     }
     
     public static function logout() {
@@ -19,12 +24,14 @@ class LoginController {
         }
     }
 
-        public static function crear() {
-            echo "Desde Crear";
+        public static function crear(Router $router) {
     
             if($_SERVER['REQUEST_METHOD'] === 'POST') {
                 
             }
+            $router->render('auth/crear', [
+                'titulo' => 'Crear Cuenta en UpTask'
+            ]);
         }
 
         public static function olvide() {
---
+++ views/auth/login.php
@@ -0,0 +1,40 @@
+<div class="contenedor login">
+    <h1 class="uptask">UpTask</h1>
+    <p class="tagline">Crea y Administra tus Proyectos</p>
+
+    <div class="contenedor-sm">
+
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+
+        <form action="/" class="formulario" method="POST">
+
+            <div class="campo">
+                <label for="email">Email</label>
+                <input 
+                    type="email"
+                    id="email"
+                    placeholder="Tu Email"
+                    name="email"
+                />
+            </div>
+
+            <div class="campo">
+                <label for="password">Password</label>
+                <input 
+                    type="password"
+                    id="password"
+                    placeholder="Tu Password"
+                    name="password"
+                />
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar Sesión">
+
+            <div class="acciones">
+                <a href="/crear">¿Aún no tienes cuenta? Crear una</a>
+                <a href="/olvide">¿Olvidaste tu Password?</a>
+            </div>
+
+        </form>
+    </div> <!--.contenedor-sm-->
+</div>
